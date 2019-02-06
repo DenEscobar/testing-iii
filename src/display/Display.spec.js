d2d74274f2805e1dafa6c3b792201d69f79990ba
Denise Escobar <descobar1490@gmail.com>
2019-02-05 22:28:35 -0600

    have a test to check if the closed and locked prop work for false in the display component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+//displays if gate is open/closed and if it is locked/unlocked
+//displays 'Closed' if the closed prop is true and 'Open' if otherwise
+//displays 'Locked' if the locked prop is true and 'Unlocked' if othewise
+//when locked or closed use the red-led class
+//when unlocked or open use the green-led class
+
+import React from 'react';
+import { render } from 'react-testing-library';
+
+import Display from './Display';
+
+describe('The display component', () =>{
+    it('displays if the gate is open or closed', () =>{
+        const {getByText} = render(<Display locked ={false} closed ={false}/> );
+
+        getByText(/Unlocked/i);
+        getByText(/Open/i);
+    })
+})
